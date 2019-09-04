e8ec166be8f86c4e1fd627c4e81e8c64443f6d73
Lucas de Paula <lucas.barbosa@microcity.com.br>
2019-09-04 19:56:46 -0300

    surra

--- redux-shopping-cart/src/components/CompInitialPage.js
+++ redux-shopping-cart/src/components/CompInitialPage.js
@@ -1,4 +1,5 @@
-import React from "react";
+// import React from "react";
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import { Card } from "./CompCard";
 import { Products } from "../api/Products";
@@ -10,25 +11,57 @@ import { Products } from "../api/Products";
 
 
 export function InitialPage() {
-    let objeto = Products.getProductById(2).then((obj) => {
-        // alert("teste2: " + obj.name);
-        return obj;
-    })
+    //let objeto = 'gggg';
+
+    // function teste(valor1){
+    //    return objeto = valor1;
+    // }
+
+    // Products.getProductById(2).then((obj) => {
+    //     //  alert("teste2: " + obj.name);
+    //     // objeto = obj.name;
+    //     teste(obj);
+    // })
+
+    // alert("teste2: " + objeto);
+
+    let objeto = await Products.getProductById(1);
+
+    alert(objeto.name);
 
     return (
         <>
             <div className="App">
                 <main className="App-header">
                     <h4>Amazing redux store</h4>
-              
 
-                        <Card objeto />
-              
-                    
+
+                    {/* <Card obj={objeto} /> */}
+
 
 
                 </main>
             </div>
         </>
     )
+
+    // const [data, setData] = useState({ items: [] });
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const result = await Products.getProducts();
+    //     setData(result.data);
+    //     };
+    //     fetchData();
+    // }, []);
+
+    // return (
+    //   <ul>
+    //     {data.items.map(item => (
+    //       <li key={item.name}>
+    //         <a href={item.image}>{item.description}</a>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // );
 }
